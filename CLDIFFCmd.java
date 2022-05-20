c482bf4fe66f7c512bbe44bf40c1548d8da1be91
JunYuWang0808 <47069435+JunYuWang0808@users.noreply.github.com>
2022-05-20 11:22:07 -0700

    Update CLDIFFCmd.java

--- CLDIFFCmd.java
+++ CLDIFFCmd.java
@@ -2,7 +2,9 @@ public class CLDIFFCmd {
   public void quickSort1(int arr[], int begin, int end) {
       int end = 1;
     
-      begin = end + 1;
+      int x = end + 5;
+    
+      begin = x + 1;
   }
 
     public static void main(String args[]){
