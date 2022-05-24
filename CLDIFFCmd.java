b83cee336d5117c8958c749faeee80482f957e74
JunYuWang0808 <47069435+JunYuWang0808@users.noreply.github.com>
2022-05-24 11:08:49 -0700

    Update CLDIFFCmd.java

--- CLDIFFCmd.java
+++ CLDIFFCmd.java
@@ -1,11 +1,38 @@
 public class CLDIFFCmd {
-  public void quickSort1(int arr[], int begin, int end) {
-      int end = 1;
-    
-      int x = end + 5;
-    
-      begin = x + 1;
+  public int keyDownEvent (int wParam, int lParam) {
+       ExpandItem item = items [focusIndex];
+       switch (wParam) {
+         case OS.VK_SPACE:
+         case OS.VK_RETURN:
+         Event event = new Event ();
+         event.item = item;
+         sendEvent(true, event);
+       }
   }
+  
+  
+ public int keyReleaseEvent (int wParam, int lParam) {
+   ExpandItem item = items [focusIndex];
+   switch (wParam) {
+     case OS.GDK_RETURN:
+     case OS.GDK_SPACE:
+     Event ev = new Event ();
+     ev.item = item;
+     sendEvent(true, ev);
+   }
+  }
+  
+ public int buttonUpEvent (int wParam, int lParam) {
+   ExpandItem item = items [focusIndex];
+   if (lParam == HOVER) {
+   Event bEvent = new Event ();
+   bEvent.item = item;
+   sendEvent(true, bEvent);
+   }
+  }
+  
+  
+  
 
     public static void main(String args[]){
         
